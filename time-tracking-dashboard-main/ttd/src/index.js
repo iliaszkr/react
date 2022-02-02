349daf0b4fd2b050276419f50b71ae9956c7ce2f
Ilias <ilias.zkiri@outlook.com>
2022-02-02 11:29:35 +0100

    update

---
+++ time-tracking-dashboard-main/ttd/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+import './sass/style.sass'
+import "@fortawesome/fontawesome-free/css/all.css"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+
+
+
